29c273cb58f415f461ada88e54c4cbded41c4cbc
jaaborot <jaaborot@up.edu.ph>
2017-04-27 18:04:47 +0800

    Adding files amplification.py and qram.py to GitHub. Just came back after a looong time. Fuck!

---
+++ qram.py
@@ -0,0 +1,13 @@
+# Author: Jeffrey A. Aborot
+# Affiliation 1: Advanced Science and Technology Institute
+#               Department of Science and Technology
+#               Republic of the Philippines
+#
+# Affiliation 2: Algorithms and Complexity Laboratory
+#                Department of Computer Science
+#                University of the Philippines Diliman
+#
+# Description: This python source code implements a simulation of the qRAM model of memory by Giovanetti et al., 2008.
+
+# TODO: Implement storage of substrings in a qRAM.
+# TODO: Implement fetch of data from qRAM using address register and output register.
