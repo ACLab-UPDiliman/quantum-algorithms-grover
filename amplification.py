560103e877496b92b2be8bf12ade870b39e1c41a
jaaborot <jaaborot@up.edu.ph>
2017-04-27 18:04:03 +0800

    Adding files amplification.py and qram.py to GitHub. Just came back after a looong time. Fuck!

---
+++ amplification.py
@@ -0,0 +1,8 @@
+# Author: Jeffrey A. Aborot
+# Description: This quantum algorithm uses Grover's quantum algorithm as a scheme. It provides a quantum circuit design of the oracle for string matching.
+
+# TODO: Implement superposition operation on 00 state.
+# TODO: Implement simulation of a qRAM.
+# TODO: Implement marking operation on superposition state.
+# TODO: Implement amplitude amplification of marked state.
+# TODO: Implement measurement operation.
